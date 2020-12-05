8cc01e7ae77ed53071ae2054b620b5037eaf8445
greengnat <will.lucas1996@gmail.com>
2020-12-05 09:45:19 -0800

    continuing with redux

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,8 @@
 import userReducer from "./user.reducer";
 
+import { UserActionTypes } from "./user.types";
+
 export const setCurrentUser = (user) => ({
-  type: "SET_CURRENT_USER",
+  type: UserActionTypes.SET_CURRENT_USER,
   payload: user,
 });
