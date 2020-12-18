cbb97bd9a6ca3c736b95553058e59e0df58a0e35
william lucas <will.lucas1996@gmail.com>
2020-12-18 10:09:20 -0800

    learning about currying, setting up collection routing and selector

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,6 +1,10 @@
 import React from 'react' 
+import { connect } from 'react-redux'
+
 
 import CollectionItem from '../../components/collection-item/collection-item.component'
+
+import { selectCollection } from '../../redux/shop/shop.selectors'
 
 import './collection.styles.scss'
 
@@ -11,4 +15,8 @@ const CollectionPage = ({ match }) => (
     </div>
 )
 
-export default CollectionPage
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage)
